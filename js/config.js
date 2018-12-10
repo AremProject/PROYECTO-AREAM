641fd26e01f8bfd9cebafd340a5bf1f445b013b9
nicolas patino <nicolas.patino@mail.escuelaing.edu.co>
2018-12-09 22:04:10 -0500

    conectar

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_T9oGiqrXo', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6pamt48uk978v0cdqgm8bk0pt4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g.https://5mfkhbyanh.execute-api.us-east-2.amazonaws.com/prod',
+    }
+};
---
+++ js/options.js
@@ -0,0 +1,14 @@
+var WildRydes = window.WildRydes || {};
+(function rideScopeWrapper($) {
+    var authToken;
+    WildRydes.authToken.then(function setAuthToken(token) {
+        if (token) {
+            authToken = token;
+        } else {
+            window.location.href = '/signin.html';
+        }
+    }).catch(function handleTokenError(error) {
+        alert(error);
+        window.location.href = '/signin.html';
+    });
+}
